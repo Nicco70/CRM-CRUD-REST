e4d7455ddd6f8150516c2dfdd0265d0b76fc2747
nicco70 <nicolasgf_98@hotmail.com>
2023-05-01 20:04:39 -0300

    commit final CRM CRUD con REST

---
+++ js/editarcliente.js
@@ -0,0 +1,52 @@
+import {obtenerCliente, editarCliente } from './API.js';
+import { mostrarAlerta, validar } from './funciones.js'
+
+(function() {
+    // Campos del formulario
+    const nombreInput = document.querySelector('#nombre');
+    const emailInput = document.querySelector('#email');
+    const telefonoInput = document.querySelector('#telefono');
+    const empresaInput = document.querySelector('#empresa');
+    const idInput = document.querySelector('#id');
+
+    document.addEventListener('DOMContentLoaded', async () => {
+        // Verificar si el cliente existe y extraigo su id
+        const parametrosURL = new URLSearchParams(window.location.search);
+        const idCliente = parseInt( parametrosURL.get('id') );
+        
+        const cliente = await obtenerCliente(idCliente)
+        mostrarCliente(cliente);
+       
+        // Submit al formulario de la edición 
+        const formulario = document.querySelector('#formulario');
+        formulario.addEventListener('submit', validarCliente);
+       
+    });
+
+    function mostrarCliente(cliente) {
+        const { nombre, empresa, email, telefono, id} = cliente;
+
+        nombreInput.value = nombre;
+        empresaInput.value = empresa;
+        emailInput.value = email;
+        telefonoInput.value = telefono;
+        idInput.value = id;
+    }
+
+    async function validarCliente(e) {
+        e.preventDefault();
+        const cliente = {
+            nombre: nombreInput.value, 
+            email: emailInput.value, 
+            telefono: telefonoInput.value,
+            empresa: empresaInput.value,
+            id: parseInt(idInput.value)
+        }
+        if( validar(cliente) ) {
+            mostrarAlerta('Todos los campos son obligatorios');
+            return;
+        }
+        // Reescribe el objeto
+        editarCliente(cliente);
+    }
+})();
